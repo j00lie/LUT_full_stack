9601fccc83cec4af14ea9de766c7ad91b4ce5cde
j00lie <joonas.liedes@student.lut.fi>
2022-06-15 12:48:09 +0300

    express crash course

--- Coursework/index.js
+++ Coursework/index.js
@@ -1,102 +1,36 @@
-/*console.log('Hello from node');
-
-
-//import object literal
-//const person = require('./person');
-//console.log(person);
-
-//import class
-
-const Person = require('./person');
-const p1 = new Person('John', 30);
-console.log(p1.name);
-p1.greeting()
-
-//create logger from logger.js
-const Logger = require('./logger');
-
-const logger = new Logger(); 
-
-logger.on('message', (data)=>console.log('Called Listener: ', data)); 
-
-logger.log('Hello world'); */
-
-const http = require("http");
+const express = require("express");
 const path = require("path");
-const fs = require("fs");
+const exphbs = require("express-handlebars");
+const logger = require("./middleware/logger");
+const members = require("./Members");
+const app = express();
 
-const server = http.createServer((req, res) => {
-  /*
-    if(req.url==='/'){
-        fs.readFile(path.join(__dirname, 'public', 'index.html'),(err, content)=>{
-            if(err) throw err; 
-            res.writeHead(200, {'Content-Type': 'text/html'})
-            res.end(content);
-        })
-    }
-    if(req.url==='/api/users'){
-        const users = [
-            {name: 'Bob Smith', age: 40}, 
-            {name:'John  Doe', age : 30}
-        ];
-        res.writeHead(200, {'Content-Type': 'application/json'}); 
-        res.end(JSON.stringify(users));
-    }
-    */
-  //Dynamic file path
-  let filepath = path.join(
-    __dirname,
-    "public",
-    req.url === "/" ? "index.html" : req.url
-  );
-  //extension of  file
-  let extname = path.extname(filepath);
-  //init content type
-  let contentType = "text/html";
+//init middleware
+//app.use(logger);
 
-  switch (extname) {
-    case ".js":
-      contentType = "text/javascript";
-      break;
-    case ".css":
-      contentType = "text/css";
-      break;
-    case ".json":
-      contentType = "application/json";
-      break;
-    case ".png":
-      contentType = "image/png";
-      break;
-    case ".jpg":
-      contentType = "image/jpg";
-      break;
-  }
+//handlebars middleware
 
-  //read file
-  fs.readFile(filepath, (err, content) => {
-    if (err) {
-      if (err.code == "ENOENT") {
-        //page not found
-        fs.readFile(
-          path.join(__dirname, "public", "404.html"),
-          (err, content) => {
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.end(content, "utf8");
-          }
-        );
-      } else {
-        //some server error
-        res.writeHead(500);
-        res.end(`Server error: ${err.code} `);
-      }
-    } else {
-      //succes
-      res.writeHead(200, { "Content-Type": contentType });
-      res.end(content, "utf8");
-    }
-  });
-});
+app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//homepage route
+app.get("/", (req, res) =>
+  res.render("index", {
+    title: "Member app",
+    members,
+  })
+);
+
+//set static folder with use()
+app.use(express.static(path.join(__dirname, "public")));
+
+//Members API routes
+app.use("/api/members", require("./routes/api/members"));
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
