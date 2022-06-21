60be426fba32691ecc6eef88464d87da161b6ba1
j00lie <joonas.liedes@student.lut.fi>
2022-06-21 14:58:22 +0300

    login and logout

--- Project/angular-src/src/app/components/register/register.component.ts
+++ Project/angular-src/src/app/components/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { ValidateService } from "app/services/validate.service";
 import { FlashMessagesService } from "angular2-flash-messages";
+import { AuthService } from "app/services/auth.service";
+import { Router } from "@angular/router";
 @Component({
   selector: "app-register",
   templateUrl: "./register.component.html",
@@ -14,7 +16,9 @@ export class RegisterComponent implements OnInit {
 
   constructor(
     private validateService: ValidateService,
-    private flashMessage: FlashMessagesService
+    private flashMessage: FlashMessagesService,
+    private authService: AuthService,
+    private router: Router
   ) {}
 
   ngOnInit() {}
@@ -44,5 +48,21 @@ export class RegisterComponent implements OnInit {
       });
       return false;
     }
+    //Register user
+    this.authService.registerUser(user).subscribe((data) => {
+      if (data.success) {
+        this.flashMessage.show("You're now registered and can log in", {
+          cssClass: "alert-success",
+          timeout: 3000,
+        });
+        this.router.navigate(["/login"]);
+      } else {
+        this.flashMessage.show("Something went wrong", {
+          cssClass: "alert-danger",
+          timeout: 3000,
+        });
+        this.router.navigate(["/register"]);
+      }
+    });
   }
 }
